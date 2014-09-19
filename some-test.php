15a3f4cce75278aa5a662d97dbf4d2a0f23efb92
buildsample <buildsampletest@gmail.com>
2014-09-19 20:37:26 +0530

    Update some-test.php

--- some-test.php
+++ some-test.php
@@ -4,7 +4,7 @@ class SQLTest extends PHPUnit_Framework_TestCase {
   public function test() {
     $sql = new SQL();
     $doc = $sql->main();
-    $this->assertNotEquals($doc, false);
+    $this->assertNotEquals($docc, false);
   }
 }
 ?>

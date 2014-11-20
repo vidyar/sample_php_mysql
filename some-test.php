f28713d006045c8b660f53f7b34d6858b9908271
buildsample <buildsampletest@gmail.com>
2014-11-20 15:37:57 +0530

    [shippable new build status flow]

--- some-test.php
+++ some-test.php
@@ -4,6 +4,7 @@ class SQLTest extends PHPUnit_Framework_TestCase {
   public function test() {
     $sql = new SQL();
     $doc = $sql->main();
+    
     $this->assertNotEquals($doc, false);
   }
 }
